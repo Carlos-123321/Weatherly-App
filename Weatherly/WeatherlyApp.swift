af255b7235639ed0318e2b2da155a08cb8bb80d1
Carlos-123321 <carlosfreestyle12@gmail.com>
2024-12-07 15:56:59 -0500

    Initial Commit

---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Karla Melisa Alvarado Sanchez on 2024-12-07.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherlyTests/WeatherlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyTests.swift
+//  WeatherlyTests
+//
+//  Created by Karla Melisa Alvarado Sanchez on 2024-12-07.
+//
+
+import Testing
+@testable import Weatherly
+
+struct WeatherlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
